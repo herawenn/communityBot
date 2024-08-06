5330f87162d27cce934c6b54fb35dc21960ce4a8
Alex Terrible <86304882+herawenn@users.noreply.github.com>
2024-08-06 09:51:58 -0400

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,15 +1,13 @@
 from discord.ext import commands
-from bot import has_restricted_role
-
+from bot import logger
 
 class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="ping", help="Displays the bot's latency.")
-    @has_restricted_role()
     async def ping_command(self, ctx):
-        await ctx.send(f"Ping: {self.bot.latency * 1000:.2f}ms")
+        await ctx.send(f"`Ping: {self.bot.latency * 1000:.2f}ms`")
 
 
 async def setup(bot):
