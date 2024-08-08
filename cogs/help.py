fb2bdafe5320da9dfbf2c08cf202fd249feb0d46
Alex Terrible <86304882+herawenn@users.noreply.github.com>
2024-08-08 10:59:01 -0400

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -9,27 +9,32 @@ class Help(commands.Cog):
         self.bot = bot
 
     @commands.command(name="help", help="Displays information about the bot and its commands.")
-    async def help_command(self, ctx):
+    async def help(self, ctx):
         try:
-            embed = discord.Embed(
-                title="Community Help Command",
-                description="Here are the available commands:",
-                color=discord.Color(0x6900ff),
-            )
-
-            for cmd in self.bot.commands:
-                embed.add_field(
-                    name=f"`{ctx.prefix}{cmd.name}`",
-                    value=f"**:** `{cmd.help or 'No description available.'}`",
-                    inline=False
-                )
-
-            embed.set_image(url="https://i.imgur.com/5mp2Siz.png")
-            embed.set_footer(text="From PortLords w Love 2024")
-
+            embed = self.create_help_embed(ctx)
             await ctx.send(embed=embed)
         except Exception as e:
+            logger.error(f"An error occurred while executing the help command: {e}")
             await ctx.send("An error occurred while executing the help command. Please try again later.")
+
+    def create_help_embed(self, ctx):
+        embed = discord.Embed(
+            title="Community Help Command",
+            description="Here are the available commands:",
+            color=discord.Color(0x6900ff),
+        )
+
+        for cmd in self.bot.commands:
+            embed.add_field(
+                name=f"`{ctx.prefix}{cmd.name}`",
+                value=f"**:** `{cmd.help or 'No description available.'}`",
+                inline=False
+            )
+
+        embed.set_image(url="https://i.imgur.com/5mp2Siz.png")
+        embed.set_footer(text="From PortLords w Love 2024")
+
+        return embed
 
 async def setup(bot):
     await bot.add_cog(Help(bot))
