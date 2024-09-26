6902cb55a0efdedd4c17b4b62625f92d4a056d9d
Alex Terrible <86304882+herawenn@users.noreply.github.com>
2024-09-26 11:17:05 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,6 +1,10 @@
 import asyncio
-import discord
 import logging
+import discord
+import re
+import subprocess
+import json
+import xml.etree.ElementTree as ET
 
 logger = logging.getLogger(__name__)
 
@@ -20,3 +24,52 @@ async def send_and_delete(ctx, content=None, embed=None, file=None, delay=None, 
             return message
     except Exception as e:
         logger.error(f"An error occurred in send_and_delete: {e}")
+
+def is_valid_integer(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+def is_valid_ip_address(ip):
+    regex = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    if re.match(regex, ip):
+        return True
+    else:
+        return False
+
+def validate_scan_id(scan_id):
+    try:
+        int(scan_id)
+        return True
+    except ValueError:
+        return False
+
+def validate_exploit_id(exploit_id):
+    try:
+        int(exploit_id)
+        return True
+    except ValueError:
+        return False
+
+def create_embed(title, description, color_key='primary', fields=None, footer_text=None, image_url=None, config=None):
+    if config is None:
+        logger.error("Error: Configuration not provided to create_embed")
+        return None
+
+    embed = discord.Embed(title=title, description=description, color=int(config['embeds']['embed_colors'][color_key], 16))
+
+    if footer_text is None:
+        footer_text = config['embeds']['embed_footer']
+    embed.set_footer(text=footer_text)
+
+    if image_url is None:
+        image_url = config['embeds']['embed_banner']
+    embed.set_image(url=image_url)
+
+    if fields:
+        for field in fields:
+            embed.add_field(name=field[0], value=field[1], inline=field[2] if len(field) > 2 else False)
+
+    return embed
